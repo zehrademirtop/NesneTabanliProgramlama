6fd417fb56c4a447eed2623275d3093f87565e5b
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-08 23:19:12 +0300

    Modul1Unite1 eklendi

---
+++ Modul1Unite1/Modul1Unite1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modul1Unite1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Haftanın günlerini alt alta yazan program
+            Console.WriteLine("Pazartesi");
+            Console.WriteLine("Salı");
+            Console.WriteLine("Çarşamba");
+            Console.WriteLine("Perşembe");
+            Console.WriteLine("Cuma");
+            Console.WriteLine("Cumartesi");
+            Console.WriteLine("Pazar");
+
+            Console.ReadLine(); // Ekranın hemen kapanmaması için
+        }
+    }
+}
