5b21db5a18ab3f2ff76a87e1a9b4f2656f8107a0
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-09 23:25:56 +0300

    Modul2Unite2

---
+++ Modul2Unite2/Modul2Unite2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Modul2Unite2
+{
+    public partial class Form1 : Form
+    {
+        struct Tarih
+        {
+            public int gun;
+            public int ay;
+            public int yil;
+
+            public Tarih(int g, int a, int y)
+            {
+                gun = g;
+                ay = a;
+                yil = y;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tarih t = new Tarih(9, 5, 2025);
+            MessageBox.Show("Tarih: " + t.gun + "/" + t.ay + "/" + t.yil);
+        }
+    }
+}
