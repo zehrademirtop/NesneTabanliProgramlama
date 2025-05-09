4b6c9bd23304d63562356b05650cd3e1a9fe47b9
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-09 23:20:45 +0300

    Modul2Unite1

---
+++ Modul2Unite1/Modul2Unite1/Modul2Unite1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GunlerUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        enum Gunler
+        {
+            Pazartesi,
+            Salı,
+            Çarşamba,
+            Perşembe,
+            Cuma,
+            Cumartesi,
+            Pazar
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] gunListe = Enum.GetNames(typeof(Gunler));
+            comboBox1.Items.AddRange(gunListe);
+        }
+    }
+}
