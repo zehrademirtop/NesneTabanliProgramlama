1e1f3cee2239f382f40db7fb75aca9b2a5e35370
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-14 22:07:49 +0300

    Modul7Unite2

---
+++ Modul7Unite2/OkulYonetim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+interface IInsan
+{
+    void Buyu();
+}
+
+class Ogrenci : OkuyanInsan, IInsan
+{
+    public void Buyu()
+    {
+        Console.WriteLine("Hızlı büyüdüm.");
+    }
+}
+
+class Ogretmen : OkuyanInsan, IInsan
+{
+    public void Buyu()
+    {
+        Console.WriteLine("Yavaş büyüdüm.");
+    }
+}
+
+abstract class OkuyanInsan
+{
+    public void Oku()
+    {
+        Console.WriteLine("Okudum.");
+    }
+}
---
+++ Modul7Unite2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ogrenci birOgrenci = new Ogrenci();
+        Ogretmen birOgretmen = new Ogretmen();
+
+        birOgrenci.Buyu();
+        birOgretmen.Buyu();
+
+        birOgrenci.Oku();
+        birOgretmen.Oku();
+    }
+}
