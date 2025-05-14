6d36d616a850cf865b8db1519c7450e20c81fd22
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-14 21:20:46 +0300

    Modul5Unite2

---
+++ Modul5Unite2/Modul5Unite2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StaticOrnegi
+{
+    class Program
+    {
+        
+        class Nokta
+        {
+            private int x, y;
+            private static int noktaSayisi = 0;
+
+           
+            public Nokta()
+            {
+                this.x = 0;
+                this.y = 0;
+                noktaSayisi++;
+            }
+
+           
+            public Nokta(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                noktaSayisi++;
+            }
+
+            public static int NoktaSayisi()
+            {
+                return noktaSayisi;
+            }
+
+            public void Yazdir()
+            {
+                Console.WriteLine($"Nokta: ({x}, {y})");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Nokta n1 = new Nokta();         
+            Nokta n2 = new Nokta(5, 10);   
+            Nokta n3 = new Nokta(3, 7);     
+
+           
+            n1.Yazdir();
+            n2.Yazdir();
+            n3.Yazdir();
+
+           
+            Console.WriteLine("Toplam Nokta Sayısı: " + Nokta.NoktaSayisi());
+
+            Console.ReadLine(); 
+        }
+    }
+}
