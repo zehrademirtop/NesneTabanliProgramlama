f5b86a2766c1c64d6d3a5896059ff0700a6a519b
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-14 21:26:40 +0300

    Modul6Unite1

---
+++ Modul6Unite1/Modul6Unite1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace ToplamaIslemi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void islem(object sender, RoutedEventArgs e)
+        {
+
+            int sayi1, sayi2, sonuc;
+
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);  
+
+            sonuc = sayi1 + sayi2;
+            
+            textBox3.Text = sonuc.ToString();
+        }
+    }
+}
