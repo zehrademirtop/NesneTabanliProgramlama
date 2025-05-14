730436c2b28ace3386cc8e6b622312dbc2e6032c
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-14 21:07:06 +0300

    Modul4Unite2

---
+++ Modul4Unite2/Modul4Unite2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace refOrnegi
+{
+    class Program
+    {
+        
+        static void DegerArtir(ref int sayi)
+        {
+            sayi = sayi + 1;
+        }
+
+        static void Main(string[] args)
+        {
+           
+            int sayim = 10;
+
+            
+            DegerArtir(ref sayim);
+
+            
+            Console.WriteLine("Sayı artırıldıktan sonra değeri: " + sayim);
+
+            Console.ReadLine(); 
+        }
+    }
+}
