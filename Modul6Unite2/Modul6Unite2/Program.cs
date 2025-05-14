9164164cda257176ff8f5911fe51b8e0fafedfe1
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-14 21:29:53 +0300

    Modul6Unite2

---
+++ Modul6Unite2/Modul6Unite2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KareAlmaOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kareal(5));
+            Console.WriteLine(kareal(5.1));
+            Console.WriteLine(kareal("3"));
+            Console.Read();
+        }
+
+        static int kareal(int x)
+        {
+            return x * x;
+        }
+
+        static double kareal(double x)
+        {
+            return x * x;
+        }
+
+        static int kareal(string x)
+        {
+            return Int32.Parse(x) * Int32.Parse(x);
+        }
+    }
+}
