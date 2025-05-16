93531a265e90fb12036ee388b5bcd18946381c9f
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-16 23:11:12 +0300

    Modul9Unite1

---
+++ Modul9Unite1/Modul9Unite1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DiziToplamUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int[] sayi = { 1, 2, 3, 4 };
+
+            int toplam = 0;
+
+
+            foreach (int i in sayi)
+            {
+                toplam = toplam + i;
+            }
+
+            Console.WriteLine("Dizi Elemanlarının Toplamı: " + toplam);
+
+       
+            Console.ReadLine();
+        }
+    }
+}
