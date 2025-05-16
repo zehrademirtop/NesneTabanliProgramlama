b25d9a6dd218310ee3d7fefafcb0d4078b31a2aa
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-16 23:16:16 +0300

    Modul9Unite2

---
+++ Modul9Unite2/Modul9Unite2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections; 
+
+namespace ArrayListOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sayi = new ArrayList();
+
+            
+            int[] num = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < 10; i++)
+            {
+                sayi.Add(num[i]);
+            }
+
+         
+            sayi.RemoveAt(2);
+
+            sayi.Insert(5, 2345); 
+
+           
+            Console.WriteLine("ArrayList Elemanları:");
+            for (int i = 0; i < sayi.Count; i++)
+            {
+                Console.WriteLine(sayi[i]);
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
