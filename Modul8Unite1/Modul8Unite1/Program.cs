e907a83ebe10f450bd6eee4c4dc2a01fee11fc60
zehrademirtop <zehrademirtop08@gmail.com>
2025-05-16 22:56:14 +0300

    Modul8Unite1

---
+++ Modul8Unite1/Modul8Unite1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TemsilciOrnegi
+{
+
+    public delegate void DiziIsle(ref string[] dizi);
+
+    class Program
+    {
+      
+        public static void DiziYazdir(ref string[] dizi)
+        {
+            Console.WriteLine("Dizinin Elemanları:");
+            foreach (string s in dizi)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("-------------------------");
+        }
+
+        public static void SiralaAZ(ref string[] dizi)
+        {
+            Array.Sort(dizi);
+            Console.WriteLine("A-Z'ye göre sıralandı.");
+        }
+
+
+        public static void SiralaZA(ref string[] dizi)
+        {
+            Array.Sort(dizi);
+            Array.Reverse(dizi);
+            Console.WriteLine("Z-A'ya göre sıralandı.");
+        }
+
+        static void Main(string[] args)
+        {
+          
+            string[] kelimeler = { "Portakal", "Elma", "Muz", "Karpuz", "Çilek" };
+
+            DiziIsle temsilci;
+
+            temsilci = DiziYazdir;
+            Console.WriteLine("İlk Hali:");
+            temsilci(ref kelimeler);
+
+         
+            temsilci = SiralaAZ;
+            temsilci(ref kelimeler);
+
+            temsilci = DiziYazdir;
+            temsilci(ref kelimeler);
+
+       
+            temsilci = SiralaZA;
+            temsilci += DiziYazdir;
+
+            temsilci(ref kelimeler);
+
+            Console.WriteLine("İşlem tamamlandı.");
+            Console.ReadKey();
+        }
+    }
+}
